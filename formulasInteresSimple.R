6261029e502e6210af9e8384372acd490ff47f0b
STEPHANI21 <2350491j@umich.mx>
2024-09-19 12:40:57 -0600

    Carga tarea version 1.0
    En esta carga se sube la primera version de la tarea de interes siemple

---
+++ formulasInteresSimple.R
@@ -0,0 +1,11 @@
+#Se presentan las funciones de interes simple 
+#Autor:Stephani Olvera Chavez 
+#V1.0 :19 de septiembre de 2024
+
+#Valor futuro con interes simple
+
+valorFinalSimple=function(VA,r,t){
+  xSalida=VA*(1+(r*t))
+  
+  return(xSalida)
+}
